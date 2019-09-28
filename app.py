95dcc0569fe45062cc70ec149d98c6433fe0f2b1
Bobby Georgiou <hgeorgio@stevens.edu>
2019-09-27 23:12:18 -0400

    Move orphan html

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route('/survey', methods=['GET', 'POST'])
+def survey():
+    if request.method == 'POST':
+        # do stuff when the form is submitted
+
+        # redirect to end the POST handling
+        # the redirect can be to the same route or somewhere else
+        return redirect(url_for('index'))
+
+    # show the form (wasn't submitted)
+    return render_template('survey.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
