26484d0a73cbc4c044728c85590e03eebae4b313
Philip Reilly <psr4pv@virginia.edu>
2019-09-28 00:33:40 -0400

    new ShatterMain

--- ShatterMain.cs
+++ ShatterMain.cs
@@ -13,9 +13,22 @@ public class ShatterMain : MonoBehaviour {
 		public GameObject levelobjs;
 	}
 
+	public string[] questions;
+	public string[] answer1;
+	public string[] answer2;
+	public string[] answer3;
+	public string[] answer4;
+
+	public Text[] answers;
+	public int[] cans;
+	public int canswer;
+	
+	public Text qtext;
 	public LLevel[] levels;
 
 	public int clevel;
+
+	public int score;
 
 	void Start () {
 		clevel = 0;
@@ -46,5 +59,28 @@ public class ShatterMain : MonoBehaviour {
 			}
 		}
 		clevel = nlevel;
+	}
+
+	public void GenQ ()
+	{
+		int r = Math.RandomRange(0, questions.Length-1);
+		qtext.Text = questions[r];
+		answers[0] = answer1[r];
+		answers[1] = answer2[r];
+		answers[2] = answer3[r];
+		answers[3] = answer4[r];
+		canswer = cans[r];
+	}
+
+	public void check(int inpu)
+	{
+		if(inpu == canswer)
+		{
+			score++;
+		}
+		else
+		{
+			score--;
+		}
 	}
 }
