3dee52c2f32262f3286e3087b45a2d4c8447002a
BolajiMakinde <44919483+BolajiMakinde@users.noreply.github.com>
2019-09-28 00:38:12 -0700

    Add files via upload

---
+++ game/ShatterMain.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShatterMain : MonoBehaviour {
+
+	// Use this for initialization
+	[System.Serializable]
+	public class LLevel
+	{
+		public string name;
+		public int levelnum = 0;
+		public GameObject levelobjs;
+	}
+
+	public GameObject player;
+
+	public float walkingspeed = 0;
+
+	public string[] questions;
+	public string[] answer1;
+	public string[] answer2;
+	public string[] answer3;
+	public string[] answer4;
+
+	public GameObject[] answers;
+	public int[] cans;
+	public int canswer;
+	
+	public GameObject qtext;
+
+	public GameObject scorego;
+	public LLevel[] levels;
+
+	public int clevel;
+
+	public int score;
+
+	public int scoremax;
+
+	public bool youwin = false;
+	public GameObject badge;
+
+	void Start () {
+		clevel = 0;
+		foreach(LLevel level in levels)
+		{
+			if(level.levelnum != 0)
+			{
+				level.levelobjs.SetActive(false);
+			}
+			else
+			{
+				level.levelobjs.SetActive(true);
+			}
+		}
+		int r = 0;
+		r = Random.Range(0, questions.Length);
+		qtext.GetComponent<Text>().text = questions[r];
+		answers[0].GetComponent<Text>().text = answer1[r];
+		answers[1].GetComponent<Text>().text = answer2[r];
+		answers[2].GetComponent<Text>().text = answer3[r];
+		answers[3].GetComponent<Text>().text = answer4[r];
+		scorego.GetComponent<Text>().text = ("Score: " + score);
+		
+		canswer = cans[r];
+	}
+
+	//public void FixedUpdate()
+	//{
+	//	walkingspeed = (Mathf.Sqrt(Mathf.Lerp(walkingspeed, Input.GetAxis("Vertical")**2+ Input.GetAxis("Horizontal")**2)));
+		//player.GetComponent<
+	//}
+	
+	// Update is called once per frame
+	public void UpdateLevel (int nlevel) {
+		foreach(LLevel level in levels)
+		{
+			if(nlevel != level.levelnum)
+			{
+				level.levelobjs.SetActive(false);
+			}
+			else
+			{
+				level.levelobjs.SetActive(true);
+			}
+		}
+		clevel = nlevel;
+		youwin = false;
+	}
+
+	public void GenQ ()
+	{
+		if(youwin == false)
+		{
+			int r = 0;
+			r = Random.Range(0, questions.Length);
+			qtext.GetComponent<Text>().text = questions[r];
+			answers[0].GetComponent<Text>().text = answer1[r];
+			answers[1].GetComponent<Text>().text = answer2[r];
+			answers[2].GetComponent<Text>().text = answer3[r];
+			answers[3].GetComponent<Text>().text = answer4[r];
+			scorego.GetComponent<Text>().text = ("Score: " + score);
+			canswer = cans[r];
+			badge.SetActive(false);
+		}
+		if(Mathf.Abs(score) > scoremax)
+		{
+			if(score < 0)
+			{
+				scorego.GetComponent<Text>().text = ("Try again next time");
+				youwin = true;
+				score = 0;
+				badge.SetActive(false);
+				qtext.GetComponent<Text>().text = "Sorry!";
+				answers[0].GetComponent<Text>().text = "Try";
+				answers[1].GetComponent<Text>().text = "again";
+				answers[2].GetComponent<Text>().text = "time";
+				answers[3].GetComponent<Text>().text = "next";
+			}
+			else
+			{
+				scorego.GetComponent<Text>().text = ("Good job! You win!");
+				youwin = true;
+				score = 0;
+				badge.SetActive(true);
+				qtext.GetComponent<Text>().text = "Congratulations";
+				answers[0].GetComponent<Text>().text = "You";
+				answers[1].GetComponent<Text>().text = "are";
+				answers[2].GetComponent<Text>().text = "winner";
+				answers[3].GetComponent<Text>().text = "the";
+			}
+		}
+	}
+
+	public void check(int inpu)
+	{
+		if(youwin == false)
+		{
+			if(inpu == canswer)
+			{
+				score++;
+			}
+			else
+			{
+				score--;
+			}
+		}
+		else
+		{
+
+		}
+	}
+}
