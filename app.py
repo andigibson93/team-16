ad97830ac381b53f159592dac266ac0c7d44beb2
Bobby Georgiou <hgeorgio@stevens.edu>
2019-09-28 02:22:00 -0400

    Game functionality

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
-badges=[]
 
 # Route for index
 @app.route("/")
@@ -13,14 +12,8 @@ def survey():
     if request.method == 'POST':
         # form submitted
         
- 
-        # redirect to end the POST handling
-        # the redirect can be to the same route or somewhere else
-        badges.add("Survey Badge")
 
- 
         # redirect after POST handling, redirect can be to the same route or somewhere else
- 
         return redirect(url_for('index'))
 
     # show the form (wasn't submitted)
@@ -28,16 +21,15 @@ def survey():
 
 @app.route('/volunteer')
 def volunteer():
-	return render_template('volunteer.html')
+    return render_template('volunteer.html')
 
 @app.route('/surveyResults')
 def surveyResults():
 	return render_template('surveyResults.html')
 
-
-@app.route('/play')
-def play():
-    return render_template('play.html')
+@app.route('/game')
+def game():
+    return render_template('game.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
